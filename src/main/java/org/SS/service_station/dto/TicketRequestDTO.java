b391c13d4daba604022f7f4365949f0c0895476b
Scarlet-Lotus <mamlukbeibarys@gmail.com>
2025-06-08 02:55:14 +0500

    Initial create

---
+++ src/main/java/org/SS/service_station/dto/TicketRequestDTO.java
@@ -0,0 +1,14 @@
+package org.SS.service_station.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TicketRequestDTO {
+
+    private String clientName; // Client Name
+
+}
