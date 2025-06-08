d879652357eff81f0be6a863e7bc45785997fc79
Scarlet-Lotus <mamlukbeibarys@gmail.com>
2025-06-08 20:17:57 +0500

    Annotations (NonNull, RequiredArgsConstructor) was added

--- src/main/java/org/SS/service_station/model/StatusChange.java
+++ src/main/java/org/SS/service_station/model/StatusChange.java
@@ -3,12 +3,15 @@ package org.SS.service_station.model;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Data // Generate getters, setters, toString, equals and hashCode
 @NoArgsConstructor // Constructor without parameters for JPA (Hibernate)
+@RequiredArgsConstructor // Constructor with required parameters
 public class StatusChange {
 
     @Id
@@ -16,18 +19,21 @@ public class StatusChange {
     private Long id;
 
     @Column(nullable = false)
+    @NonNull
     private String changedBy; // Who changed status
 
     @Column(nullable = false)
+    @NonNull
     private LocalDateTime changedAt; // When changed
 
     @Column(nullable = false)
+    @NonNull
     private String reason; // Change reason
 
-    public StatusChange(String changedBy, LocalDateTime changedAt, String reason) {
-        this.changedBy = changedBy;
-        this.changedAt = changedAt;
-        this.reason = reason;
-    }
+//    public StatusChange(String changedBy, LocalDateTime changedAt, String reason) {
+//        this.changedBy = changedBy;
+//        this.changedAt = changedAt;
+//        this.reason = reason;
+//    }
 
 }
