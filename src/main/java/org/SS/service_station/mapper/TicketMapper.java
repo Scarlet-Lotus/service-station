3d62e7d4a8589c1c099e9c102b2ce59f7a8576b5
Scarlet-Lotus <mamlukbeibarys@gmail.com>
2025-06-16 03:44:12 +0500

    UtilityClass annotation was added order to an object of this class wouldn't able to created because the main work of this class is to provide special methods.

--- src/main/java/org/SS/service_station/mapper/TicketMapper.java
+++ src/main/java/org/SS/service_station/mapper/TicketMapper.java
@@ -1,20 +1,23 @@
 package org.SS.service_station.mapper;
 
-import org.SS.service_station.dto.StatusChangeDTO;
+import lombok.experimental.UtilityClass;
+import org.SS.service_station.dto.StatusHistoryDTO;
+import org.SS.service_station.dto.StatusUpdateDTO;
 import org.SS.service_station.dto.TicketResponseDTO;
 import org.SS.service_station.model.Ticket;
 
+@UtilityClass
 public class TicketMapper {
 
     // Static method for converting Ticket entity to TicketResponseDTO
-    public static TicketResponseDTO toResponse(Ticket ticket) {
+    public TicketResponseDTO toResponse(Ticket ticket) {
         return new TicketResponseDTO(
                 ticket.getId(),
                 ticket.getClientName(),
                 ticket.getStatus().name(),
                 ticket.getCreatedAt(),
                 ticket.getHistory().stream()
-                        .map(history -> new StatusChangeDTO(
+                        .map(history -> new StatusHistoryDTO(
                                 history.getChangedBy(),
                                 history.getChangedAt(),
                                 history.getReason()
