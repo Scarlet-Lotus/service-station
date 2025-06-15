5871324b5c3dfae4ae07ed724f7751dbf3ee98b4
Scarlet-Lotus <mamlukbeibarys@gmail.com>
2025-06-16 03:31:31 +0500

    Constructor and Table's name were added

--- src/main/java/org/SS/service_station/model/StatusUpdate.java
+++ src/main/java/org/SS/service_station/model/StatusUpdate.java
@@ -3,31 +3,32 @@ package org.SS.service_station.model;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "ticket_status_history")
 @Data // Generate getters, setters, toString, equals and hashCode
 @NoArgsConstructor // Constructor without parameters for JPA (Hibernate)
-@RequiredArgsConstructor // Constructor with required parameters
-public class StatusChange {
+public class StatusUpdate {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
-    @NonNull
     private String changedBy; // Who changed status
 
     @Column(nullable = false)
-    @NonNull
     private LocalDateTime changedAt; // When changed
 
     @Column(nullable = false)
-    @NonNull
     private String reason; // Change reason
+
+    public StatusUpdate(String changedBy, LocalDateTime changedAt, String reason) {
+        this.changedBy = changedBy;
+        this.changedAt = changedAt;
+        this.reason = reason;
+    }
 
 }
