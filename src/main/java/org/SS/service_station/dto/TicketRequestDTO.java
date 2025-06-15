6cb32aac124e38a7fb029eafb53c4667edb36f31
Scarlet-Lotus <mamlukbeibarys@gmail.com>
2025-06-16 03:46:31 +0500

    New string field (reason) was added and all fields were modified with NotBlank annotation.

--- src/main/java/org/SS/service_station/dto/TicketRequestDTO.java
+++ src/main/java/org/SS/service_station/dto/TicketRequestDTO.java
@@ -1,5 +1,6 @@
 package org.SS.service_station.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class TicketRequestDTO {
 
+    @NotBlank(message = "Client name can not be null or empty")
     private String clientName; // Client Name
+
+    @NotBlank(message = "Reason can not be null or empty")
+    private String reason; // Change status reason
 
 }
